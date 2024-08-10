e89d723332181c3cd2a4188b36abc87e684aaf92
Balajibbalaguru <balajibbalaguru@gmail.com>
2024-08-10 23:12:34 +0530

    added preferences

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Signup from './components/Signup';
 import Footer from './components/Footer';
 import Hotels from './components/Hotels';
 import Chat from './components/Chat';
+import Explore from './components/explore/Explore';
 
 
 function App() {
@@ -18,7 +19,8 @@ function App() {
           <Route path='/Destination' element={<Destination/>}></Route>
           <Route path='/Login' element={<Login />}></Route>
           <Route path='/Signup' element={<Signup />}></Route>
-          <Route path='/Hotels' element={<Hotels />}></Route>
+        <Route path='/Hotels' element={<Hotels />}></Route>
+        <Route path='/Explore' element={<Explore />}></Route>
       </Routes>
       <Chat />
       <Footer />
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 
 const Landing = () => {
   return (
@@ -5,7 +6,7 @@ const Landing = () => {
     <div className="content">
         <h1>Discover Your Next Adventure</h1>
         <p>Explore the world with personalized itineraries and AI-powered recommendations.</p>
-        <button className="start-btn">Start Planning</button>
+        <Link className=" start-plan" to="/explore"><button className="start-btn">Start Planning</button></Link>
     </div>
     </>
   )
